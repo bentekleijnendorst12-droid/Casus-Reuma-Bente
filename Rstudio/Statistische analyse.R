2ac9aa5fef4494213813a3771ac3318f2a04804d
bentekleijnendorst12-droid <bentekleijnendorst12@gmail.com>
2026-05-28 22:00:37 +0200

    Create Statistische analyse.R

---
+++ Rstudio/Statistische analyse.R
@@ -0,0 +1,11 @@
+# metadata
+Status=c ("Gezond", "Gezond", "Gezond", "Gezond", "Reuma", "Reuma", "Reuma", "Reuma")
+Status_table= data.frame(Status)
+
+#desseq dataset
+ddsR = DESeqDataSetFromMatrix(countData = echtedata,
+                              colData = Status_table,
+                              design = ~ Status)
+ddsR = DESeq(ddsR)
+resultatenR= results(ddsR)
+resultatenR
